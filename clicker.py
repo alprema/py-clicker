189a86a6bfb9ab6f0c7083e9b6bb37df8d9122ce
alprema <alprema@alprema.com>
2019-08-17 07:21:48 +0200

    Switch to pigpio library

--- clicker.py
+++ clicker.py
@@ -1,10 +1,9 @@
-from gpiozero import Button
+import pigpio
 from timeit import default_timer as timer
-import time
 import requests
 import yaml
 import asyncio
-from digital_display import start_display_loop, stop_display_loop, display_string
+from display import Display
 
 RED = 'Red'
 BLUE = 'Blue'
@@ -17,42 +16,55 @@ with open("clicker_config.yaml", 'r') as stream:
 
 print('Configured with identifier: ' + configuration['clicker-identifier'])
 
+pi = pigpio.pi()
+display = Display(pi)
 loop = asyncio.get_event_loop()
-press_times = { RED: timer(), BLUE: timer() }
 scores = { RED: 0, BLUE: 0 }
 
+
 def button_pressed(color):
-	now = timer()
-	elapsed = now - press_times[color]
-	press_times[color] = now
-	if elapsed < 0.15: # Prevents duplicate counts since the buttons can sometimes activate twice without clicking
-		return
 	scores[color] += 1
 	update_score()
-	print('Button pressed (%s), calling URL' % color + ' ' + str(elapsed))
+	print('Button pressed (%s), calling URL' % color)
 	loop.call_soon_threadsafe(lambda _: requests.get(configuration['outgoing-url'].format(clicker_identifier=configuration['clicker-identifier'], color=color)), '')
 
-def reset_pressed():
-    print('RESET PRESSED')
-    scores[RED] = 0
-    scores[BLUE] = 0
-    update_score()
+
+_last_reset_press_tick = pi.get_current_tick()
+
+def reset_pressed(gpio, level, tick):
+    global _last_reset_press_tick
+    if level == 0:
+        _last_reset_press_tick = tick
+        pi.set_watchdog(gpio, 1000)
+        return
+    
+    pi.set_watchdog(gpio, 0)
+    
+    if level == 2:
+        pressed_time_micros = pigpio.tickDiff(_last_reset_press_tick, tick)
+        if pressed_time_micros < 1000 * 1000:
+            return
+        print('RESET PRESSED')
+        scores[RED] = 0
+        scores[BLUE] = 0
+        update_score()
 
 def update_score():
-	display_string(f"{scores[RED]:02}{scores[BLUE]:02}")    
+	display.show(f"{scores[RED]:02}{scores[BLUE]:02}")    
 
-red_button = Button(4)
-red_button.when_pressed = lambda _: button_pressed(RED)
+def _setup_switch(pin: int, callback, edge=pigpio.FALLING_EDGE):
+    pi.set_mode(pin, pigpio.INPUT)
+    pi.set_pull_up_down(pin, pigpio.PUD_UP)
+    pi.set_glitch_filter(pin, 20 * 1000) # Time in µs
+    pi.callback(pin, edge, callback)
 
-blue_button = Button(18)
-blue_button.when_pressed = lambda _: button_pressed(BLUE)
+_setup_switch(4, lambda gpio, level, tick: button_pressed(RED))
+_setup_switch(18, lambda gpio, level, tick: button_pressed(BLUE))
+_setup_switch(23, reset_pressed, edge=pigpio.EITHER_EDGE)
 
-green_button = Button(23)
-green_button.hold_time = 1 
-green_button.when_held = lambda _: reset_pressed()
 
-display_string("0000")
-start_display_loop()
+display.show("0000")
+
 
 try:
     loop.run_forever()
@@ -60,5 +72,6 @@ except KeyboardInterrupt:
     print("Exiting")
     loop.close()
 
-stop_display_loop()
+display.shutdown()
+pi.stop()
 
---
+++ display.py
@@ -0,0 +1,90 @@
+import pigpio
+
+class Display:
+    # Refresh time between pulses in µs
+    REFRESH=1000
+
+    CHARSET={
+    ' ': 0b00000000,
+    '0': 0b11111100,
+    '1': 0b01100000,
+    '2': 0b11011010,
+    '3': 0b11110010,
+    '4': 0b01100110,
+    '5': 0b10110110,
+    '6': 0b10111110,
+    '7': 0b11100000,
+    '8': 0b11111110,
+    '9': 0b11110110,
+    }
+
+    # Mapping between segments and GPIO ports
+    #                   a   b   c   d   e   f   g  dp
+    segments_pinouts=[ 25, 12, 6, 22,  5, 24, 13, 19]
+
+    # Mapping between digits and GPIO ports
+    #                  1   2   3   4
+    digits_pinouts=[ 16, 20, 21, 17]
+    
+    def __init__(self, pi):
+        self.pi = pi
+        
+        for segment in self.segments_pinouts:
+           self.pi.set_mode(segment, pigpio.OUTPUT)
+
+        for digit_index in self.digits_pinouts:
+           self.pi.set_mode(digit_index, pigpio.OUTPUT)
+
+        self.current_waveform_id = None
+        self.current_display = [0]*len(self.digits_pinouts)
+
+    def show(self, display_string: str):
+        if len(display_string) != 4:
+            raise ValueError(f"Expected length value to be 4, got{len(display_string)}")
+        for i, char in enumerate(display_string):
+            if char in self.CHARSET:
+              self.current_display[i] = self.CHARSET[char]
+            else:
+              self.current_display[i] = 0
+            
+        self._update_display()
+     
+    def shutdown(self):
+        self.show("    ")
+        self.pi.wave_tx_stop()
+        self.pi.wave_delete(self.current_waveform_id)
+
+    def _update_display(self):
+        waveform = []
+        for digit_index in range(len(self.digits_pinouts)):
+
+            segments = self.current_display[digit_index] # segments on for current digit
+
+            on_mask = 0 # gpios to switch on
+            off_mask = 0 # gpios to switch off
+
+            # Set this digit on, others off
+            for d in range(len(self.digits_pinouts)):
+                if d == digit_index:
+                    off_mask |= 1<<self.digits_pinouts[d] # Digits are enabled when their pin is grounded
+                else:
+                    on_mask |= 1<<self.digits_pinouts[d]
+                    
+
+            # set used segments on, unused segments off
+            for segment_index in range(8):
+                if segments & 1<<(7-segment_index):
+                    on_mask |= 1<<self.segments_pinouts[segment_index] # switch segment on
+                else:
+                    off_mask |= 1<<self.segments_pinouts[segment_index] # switch segment off
+
+            waveform.append(pigpio.pulse(on_mask, off_mask, self.REFRESH))
+
+        self.pi.wave_add_generic(waveform) # add pulses to waveform
+        new_waveform_id = self.pi.wave_create() # commit waveform
+        self.pi.wave_send_repeat(new_waveform_id) # transmit waveform repeatedly
+
+        if self.current_waveform_id is not None:
+            self.pi.wave_delete(self.current_waveform_id) # delete no longer used waveform
+
+        self.current_waveform_id = new_waveform_id
